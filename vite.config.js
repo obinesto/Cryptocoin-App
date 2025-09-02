bf05cbae900f6115d5a38387e9babc3cd15271b1
Cyprian Obi <obicyprian180@gmail.com>
2025-09-03 00:05:39 +0100

    update favicon

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
       pwaAssets: {
-        image: 'public/assets/pwa-maskable-icon-512x512.png',
-        preset: 'minimal-2023'
+        image: "public/pwa-maskable-icon-512x512.png",
+        preset: "minimal-2023",
       },
       manifest: {
-        name: 'Cryptocoin App',
-        short_name: 'CryptoCoin',
-        description: 'Application for tracking cryptocurrency information',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        scope: '/',
-        start_url: '/',
-      }
-    })
-  ]
-})
+        name: "Cryptocoin App",
+        short_name: "CryptoCoin",
+        description: "Application for tracking cryptocurrency information",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+      },
+    }),
+  ],
+});
